a8987b64c3f5e11ee41d9e2823722da804b60acd
AlexClassicRHA <rredhackeralliance@gmail.com>
2025-03-28 00:30:03 +0300

    views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -32,16 +32,20 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (OwnerOrReadOnly,)
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return Comment.objects.filter(post_id=post_id)
 
     def get_post(self):
         return get_object_or_404(
             Post, pk=self.kwargs.get('post_id')
         )
 
-    def get_queryset(self):
-        return self.get_post().comments
+    def perform_create(self, serializer):
+        post = get_object_or_404(
+          Post, id=self.kwargs.get('post_id')
+        )
+        serializer.save(author=self.request.user, post=post)
 
 
 class FollowViewSet(mixins.CreateModelMixin,
